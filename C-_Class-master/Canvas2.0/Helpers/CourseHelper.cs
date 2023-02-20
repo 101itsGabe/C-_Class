6ef94aaf0ef177e31890d5abd1d6570f1c879360
Gabriel Mannheimer <manngabe@gmaiil.com>
2023-02-20 15:32:29 -0500

    Added Course Helper Ability to add courses to a course list

--- C-_Class-master/Canvas2.0/Helpers/CourseHelper.cs
+++ C-_Class-master/Canvas2.0/Helpers/CourseHelper.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using Objects.Models;
+using Objects.Services;
+
 namespace Canvas2._0.Helpers
 {
 	public class CourseHelper
 	{
-		public CourseHelper()
+		private CourseService cs = new CourseService();
+		public void CreateCourseRecord()
 		{
-		}
+            var newCourse = new Course();
+            Console.WriteLine("Course Name: ");
+            newCourse.Name = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Course Code: ");
+            newCourse.classCode = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Course Description: ");
+            newCourse.Description = Console.ReadLine() ?? string.Empty;
+            cs.Add(newCourse);
+        }
+
+        public void ListCourses()
+        {
+            cs.courseList.ForEach(Console.WriteLine);
+        }
 	}
 }
 
--- C-_Class-master/Objects/Services/CourseService.cs
+++ C-_Class-master/Objects/Services/CourseService.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using Objects.Models;
+
+//This is for access to the list that CourseHelper will use
 namespace Objects.Services
 {
 	public class CourseService
 	{
-		public CourseService()
-		{
-		}
-	}
+		public List<Course> courseList = new List<Course>();//doesnt have getter or setter
+
+        public void Add(Course c)
+        {
+            courseList.Add(c);
+        }
+    }
 }
 

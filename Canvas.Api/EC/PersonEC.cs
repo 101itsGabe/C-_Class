c27a0cffe516f7f1f1228a96b673cd6766555858
101itsGabe <105684932+101itsGabe@users.noreply.github.com>
2023-04-28 11:02:48 -0400

    -Canvas API

---
+++ Canvas.Api/EC/PersonEC.cs
@@ -0,0 +1,25 @@
+﻿using Objects.Models;
+using UWP.Library.Canvas.Database;
+using UWP.Library.Canvas.DTO;
+
+namespace Canvas.Api.EC
+{
+    public class PersonEC
+    {
+        public List<PersonDTO> GetPeople()
+        {
+            return DatabaseContext.People.Select(s => new PersonDTO(s)).ToList();
+        }
+
+        public PersonDTO AddOrUpdatePerson(PersonDTO p)
+        {
+            if(p.Id <= 0)
+            {
+                var lastId = DatabaseContext.People.Select(person => person.Id).Max();
+                p.Id = ++lastId;
+            }
+            DatabaseContext.People.Add(new Person(p));
+            return p;
+        }
+    }
+}
